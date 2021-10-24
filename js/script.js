9c0ac481fbc657c9942159850c602ba8cb8bf026
Anderson Mendes <mendesanderson567@gmail.com>
2021-10-24 11:28:10 -0300

    criado a cobrinha

--- js/script.js
+++ js/script.js
@@ -1,12 +1,31 @@
-console.log('hello');
+console.log('hello')
 
-let canvas = document.getElementById('snake');
-let context = canvas.getContext('2d');
-let box = 32;
+let canvas = document.getElementById('snake')
+let context = canvas.getContext('2d')
+let box = 32
 
-function criarBG() {
-  context.fillStyle = 'lightGreen';
-  context.fillRect(0, 0, 16 * box, 16 * box);
+let snake = []
+snake[0] = {
+  x: 8 * box,
+  y: 8 * box
 }
 
-criarBG();
+snake[1] = {
+  x: 7 * box,
+  y: 8 * box
+}
+
+function criaBG() {
+  context.fillStyle = 'lightGreen'
+  context.fillRect(0, 0, 16 * box, 16 * box)
+}
+
+function criaSnack() {
+  for (i = 0; i < snake.length; i++) {
+    context.fillStyle = 'green'
+    context.fillRect(snake[i].x, snake[i].y, box, box)
+  }
+}
+
+criaBG()
+criaSnack()
