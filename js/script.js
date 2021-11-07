87a40155ec0ca101bb67f0bf20e8369676f8acf1
Anderson Mendes <mendesanderson567@gmail.com>
2021-11-07 19:31:12 -0300

    definido movimentos da cobrinha

--- js/script.js
+++ js/script.js
@@ -1,23 +1,28 @@
-console.log('hello')
-
 let canvas = document.getElementById('snake')
 let context = canvas.getContext('2d')
 let box = 32
 
 let snake = []
+
 snake[0] = {
   x: 8 * box,
   y: 8 * box
 }
 
-snake[1] = {
-  x: 7 * box,
-  y: 8 * box
+let direction = 'right'
+
+function mudarDirecao(event) {
+  if (event.key == 'ArrowRight') direction = 'right'
+  if (event.key == 'ArrowLeft') direction = 'left'
+  if (event.key == 'ArrowUp') direction = 'up'
+  if (event.key == 'ArrowDown') direction = 'down'
 }
+
+window.addEventListener('keydown', mudarDirecao)
 
 function criaBG() {
   context.fillStyle = 'lightGreen'
-  context.fillRect(0, 0, 16 * box, 16 * box)
+  context.fillRect(0, 0, 16 * box, 16 * box) // posicao x, posicao y, largura, altura
 }
 
 function criaSnack() {
@@ -27,5 +32,26 @@ function criaSnack() {
   }
 }
 
-criaBG()
-criaSnack()
+function iniciarJogo() {
+  criaBG()
+  criaSnack()
+
+  let snakeX = snake[0].x
+  let snakeY = snake[0].y
+
+  if (direction == 'right') snakeX += box
+  if (direction == 'left') snakeX -= box
+  if (direction == 'up') snakeY -= box
+  if (direction == 'down') snakeY += box
+
+  snake.pop()
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY
+  }
+
+  snake.unshift(newHead)
+}
+
+let jogo = setInterval(iniciarJogo, 250) // define um intervalo de tempo depois de executado uma determinada ação
